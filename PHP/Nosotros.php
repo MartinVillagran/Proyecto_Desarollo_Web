e026f4b19c3d837862e73b237e5e7c364a4c8f85
Martín Villagrán <martinvillagran340@gmail.com>
2024-11-30 15:40:49 -0300

    Relinkeado Nosotros

---
+++ PHP/Nosotros.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nosotros</title>
+    <link rel="stylesheet" href="../CSS/style_nosotros.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid bg-dark">
+            <a class="navbar-brand text-bg-dark" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active text-bg-dark" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-bg-dark" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-bg-dark" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled text-bg-dark" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        <div class="d-flex">
+            <div class="col-8 text-left h2 mision">
+                <div class="mt-4 ms-4 fs-1 text-light">
+                    Code Titans
+                </div>
+                <div class="d-flex mt-5 ms-5 me-5 fs-5 lh-lg text-light justificar">
+                    <li></li>Nuestra misión es democratizar el acceso a la educación de calidad, ofreciendo cursos en línea diseñados para empoderar a personas de todas las edades y orígenes, ayudándoles a desarrollar habilidades clave para alcanzar sus metas personales y profesionales.
+                </div>
+            </div>
+            <img class="col-4 mb-2 img-fluid" src="../imagenes/Sonrisa.png" alt="Sonrisa" id="misionImg"> 
+        </div>
+        <div class="d-flex">
+            <img class="col-4 mb-2 img-fluid" src="../imagenes/Sonrisa.png" alt="Sonrisa" id="misionImg"> 
+            <div class="col-8 text-end h2 mision">
+                <div class="mt-4 me-4 fs-1 text-light">
+                    Code Titans
+                </div>
+                <div class="d-flex mt-5 ms-5 me-5 fs-5 lh-lg text-light justificar">
+                    <li></li>Nuestra misión es democratizar el acceso a la educación de calidad, ofreciendo cursos en línea diseñados para empoderar a personas de todas las edades y orígenes, ayudándoles a desarrollar habilidades clave para alcanzar sus metas personales y profesionales.
+                </div>
+            </div>
+        </div>
+        <div class="d-flex">
+            <div class="col-8 text-left h2 mision">
+                <div class="mt-4 ms-4 fs-1 text-light">
+                    Code Titans
+                </div>
+                <div class="d-flex mt-5 ms-5 me-5 fs-5 lh-lg text-light justificar">
+                    <li></li>Nuestra misión es democratizar el acceso a la educación de calidad, ofreciendo cursos en línea diseñados para empoderar a personas de todas las edades y orígenes, ayudándoles a desarrollar habilidades clave para alcanzar sus metas personales y profesionales.
+                </div>
+            </div>
+            <img class="col-4 mb-2 img-fluid" src="../imagenes/Sonrisa.png" alt="Sonrisa" id="misionImg"> 
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html>
